59f762bcc162c628a9d6351b520d6b7d2a6e7422
agnesaudya <70096521+agnesaudya@users.noreply.github.com>
2023-06-12 16:51:24 +0700

    add popup

--- app/src/main/java/com/example/medsafecycle/home/guest/GuestHomeActivity.kt
+++ app/src/main/java/com/example/medsafecycle/home/guest/GuestHomeActivity.kt
@@ -1,12 +1,24 @@
 package com.example.medsafecycle.home.guest
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.cardview.widget.CardView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.medsafecycle.R
@@ -16,16 +28,53 @@ import com.example.medsafecycle.landing.LandingActivity
 import com.example.medsafecycle.limbah.HistoryLimbahActivity
 import com.example.medsafecycle.limbah.HistoryLimbahAdapter
 import com.example.medsafecycle.limbah.LimbahDummy
+import java.io.File
 
 //  TODO : Buat guest, pake shared preference aja ya :) soalnya dia gapunya akun, tapi perlu history
 class GuestHomeActivity : AppCompatActivity() {
-
+    private lateinit var cameraButton: CardView
     private lateinit var rvLimbah: RecyclerView
     private lateinit var textRedirect: CardView
     private lateinit var registerRedirect: CardView
+    private var getFile: File? = null
+    companion object {
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+        private const val REQUEST_CODE_PERMISSIONS = 10
+    }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (!allPermissionsGranted()) {
+                Toast.makeText(
+                    this,
+                    "Don't get permission.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                finish()
+            }
+        }
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        if (!allPermissionsGranted()) {
+            ActivityCompat.requestPermissions(
+                this,
+                REQUIRED_PERMISSIONS,
+                REQUEST_CODE_PERMISSIONS
+            )
+        }
 
         supportActionBar?.hide()
 
@@ -33,6 +82,10 @@ class GuestHomeActivity : AppCompatActivity() {
         setUpRecyclerView()
 
         textRedirect = findViewById(R.id.redirect_nearest_hospital)
+        cameraButton = findViewById(R.id.camera_button)
+        cameraButton.setOnClickListener{
+            showImagePickerDialog()
+        }
         textRedirect.setOnClickListener {
 
             val query = "Rumah Sakit Terdekat"
@@ -71,6 +124,85 @@ class GuestHomeActivity : AppCompatActivity() {
             startActivity(moveIntent)
         }
     }
+
+    private fun showImagePickerDialog() {
+
+        val dialogView = layoutInflater.inflate(R.layout.popup_image_picker, null)
+
+        val dialogBuilder = AlertDialog.Builder(this)
+        dialogBuilder.setView(dialogView)
+
+        val  mAlertDialog = dialogBuilder.show()
+
+        val btnGallery: Button? = dialogView.findViewById(R.id.btnGallery)
+        val btnCamera = dialogView.findViewById<Button>(R.id.btnCamera)
+        val textCancel = dialogView.findViewById<TextView>(R.id.textCancel)
+
+
+        if (btnGallery != null) {
+            btnGallery.setOnClickListener {
+                Log.d("test","hei")
+                mAlertDialog.dismiss()
+                // Handle gallery button click
+    //            openGalle ry()
+
+
+            }
+        }
+
+        if (btnCamera != null) {
+            btnCamera.setOnClickListener {
+                // Handle camera button click
+    //            openCamera()
+
+            }
+        }
+
+        if (textCancel != null) {
+            textCancel.setOnClickListener {
+                // Handle cancel button click
+                mAlertDialog.dismiss()
+            }
+        }
+
+        mAlertDialog.show()
+    }
+
+    private fun startGallery() {
+        val intent = Intent()
+        intent.action = Intent.ACTION_GET_CONTENT
+        intent.type = "image/*"
+        val chooser = Intent.createChooser(intent, "Choose a Picture")
+        launcherIntentGallery.launch(chooser)
+    }
+    private val launcherIntentGallery = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val selectedImg = result.data?.data as Uri
+            selectedImg.let { uri ->
+                val myFile = uriToFile(uri, this@AddStoryActivity)
+                getFile = myFile
+                binding.previewImageView.setImageURI(uri)
+            }
+        }
+    }
+
+//    private fun startTakePhoto() {
+//        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+//        intent.resolveActivity(packageManager)
+//        createCustomTempFile(application).also {
+//            val photoURI: Uri = FileProvider.getUriForFile(
+//                this@AddStoryActivity,
+//                "com.example.mystory",
+//                it
+//            )
+//            currentPhotoPath = it.absolutePath
+//            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+//            launcherIntentCamera.launch(intent)
+//        }
+//    }
+
 
     // TODO : Jangan lupa sesuain ini sama output API, terutama bagian pas nambahin list
     private fun showRecyclerList() {
